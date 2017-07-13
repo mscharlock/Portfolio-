dc7e115a1da6cbdfde23521a8799437df5d52597
aaron mcpeek <aaronmcpeek@aarons-MacBook-Pro.local>
2017-07-13 08:00:37 -0700

    added in some methods

--- JS/projects.js
+++ JS/projects.js
@@ -3,17 +3,17 @@
 var rawData = [
   {
     projThumbnail: 'image to be added',
-    projUrl: 'url to be added',
-    title: 'Salmon Cookie',
+    projUrl: 'https://arn1313.github.io/cookie-stand/index.html',
+    title: 'Salmon Cookie'
   },
   {
     projThumbnail: 'image to be added',
-    projUrl: 'url to be added',
-    title: 'Bus Mall',
+    projUrl: 'https://arn1313.github.io/bus-mall/index.html',
+    title: 'Bus Mall'
   },
   {
     projThumbnail: 'image to be added',
-    projUrl: 'url to be added',
-    title: 'Verve.',
-  },
+    projUrl: 'https://github.com/GavinThomas1192/201-Final-Project',
+    title: 'Verve.'
+  }
 ]
