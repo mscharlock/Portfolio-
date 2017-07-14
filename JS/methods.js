d3de0c373a61dfaaa28cb7f3f48accb6364bcafd
aaron mcpeek <aaronmcpeek@aarons-MacBook-Pro.local>
2017-07-14 11:12:41 -0700

    update for day4

--- JS/methods.js
+++ JS/methods.js
@@ -4,24 +4,11 @@ var articleView = {};
 
 articleView.handleNav = function() {
   $('.tab').on('click', function() {
-    console.log('click');
     $('.tab-content').hide();
     $('#' + $(this).attr('data-content')).show();
+    console.log('click');
   });
 }
 
-articleView.handleNav();
-
 // $(document).ready(function(){
-//   $('#nav').on('click', function() {
-//     console.log('click');
-//   });
-// });
-
-
-// articleView.handleMainNav = function() {
-//   $('.tab').on('click', function() {
-//     $('.tab-content').hide();
-//     $('#' + $(this).attr('data-content')).show();
-//
-//   });
+articleView.handleNav();
