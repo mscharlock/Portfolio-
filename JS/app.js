35a3ca9e25601c5a376b84089a12ec5f1f565208
aaron mcpeek <aaronmcpeek@aarons-MacBook-Pro.local>
2017-07-16 12:05:21 -0700

    removed some extra css file

--- JS/app.js
+++ JS/app.js
@@ -14,7 +14,14 @@ projWheel.prototype.toHtml = function() {
   $newmyProjects.removeClass('proj');
 }
 
+if (!this.publishedOn) $newprojWheel.addClass('draft');
+$newprojWheel.data('category', this.category);
 
+$newprojWheel.find('h1').html(this.title);
+$newprojWheel.find('a').html(this.author);
+$newprojWheel.find('a').attr('href', this.authorUrl);
+$newprojWheel.find('time').attr('datetime', this.publishedOn);
+$newprojWheel.find('.article-body').html(this.body);
 
 
 
